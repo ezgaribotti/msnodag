2e22cc234f70a66903e0ed7c83789dde6853daed
Ezequiel Garibotti <ezgaribotti@gmail.com>
2023-11-12 12:19:31 -0300

    Servicio de operación creado

---
+++ src/Services/OperationService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services;
+
+use App\Dto\Api\OperationDto;
+use App\Entity\ServiceOperation;
+use Doctrine\ORM\EntityManagerInterface;
+
+class OperationService
+{
+    public function __construct(
+        protected EntityManagerInterface $entityManager
+    )
+    {}
+
+    public function getByName(string $name): OperationDto
+    {
+        $operation = $this->entityManager->getRepository(ServiceOperation::class)->findOneBy([
+            'name' => $name
+        ]);
+
+        if (!$operation) {
+            throw new \Exception('No se encontró la operación especificada.');
+        }
+
+        $result = new OperationDto();
+        $uri = $operation->getService()->getBaseUri() . $operation->getUri();
+        $result->setUri($uri);
+        $result->setConfigPath($operation->getService()->getConfigPath());
+        $result->setInsideKey($operation->getInsideKey());
+        $result->setResponseMap($operation->getResponseMap());
+        return $result;
+    }
+}
