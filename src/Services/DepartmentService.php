8d3b0765706b1b5c8eb72b3f18ad017468f4a96c
Ezequiel Garibotti <ezgaribotti@gmail.com>
2023-11-12 13:02:07 -0300

    Controlador de departamento creado

--- src/Services/DepartmentService.php
+++ src/Services/DepartmentService.php
@@ -2,12 +2,35 @@
 
 namespace App\Services;
 
+use App\Dto\Api\DepartmentDto;
+use App\Dto\Requests\DepartmentTransferDto;
 use Doctrine\ORM\EntityManagerInterface;
 
 class DepartmentService
 {
+    const OPERATION_NAME = 'departments';
+
     public function __construct(
-        protected EntityManagerInterface $entityManager
+        protected EntityManagerInterface $entityManager,
+        protected OperationService $operationService,
+        protected HttpService $httpService
     )
     {}
+
+    public function search(DepartmentTransferDto $data): array
+    {
+        $operation = $this->operationService->getByName(self::OPERATION_NAME);
+        $departments = $this->httpService->fetch($operation->getUri());
+        $departments = $departments[$operation->getInsideKey()];
+
+        if (!$departments) throw new \Exception('No se encontraron departamentos.', 404);
+
+        $departments = $this->httpService->format($departments, $operation->getResponseMap());
+        $departments = $this->httpService->toDto($departments, DepartmentDto::class);
+        $result = [];
+        foreach ($departments as $department) {
+            if ($department instanceof DepartmentDto) $result[] = $department->toArray();
+        }
+        return $result;
+    }
 }
